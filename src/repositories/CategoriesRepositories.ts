682110eafb857f49efd0f00643dd46221ef4b148
Felipe Moura <felipemimoura@gmail.com>
2022-07-29 20:44:02 -0300

    feat: create categories repository

---
+++ src/repositories/CategoriesRepositories.ts
@@ -0,0 +1,28 @@
+import { Category } from "../model/Category";
+
+interface ICreateCategoryDTO {
+    name: string,
+    description: string
+}
+
+class CategoriesRepositories {
+    private categories: Category[]
+
+    constructor() {
+        this.categories = []
+    }
+
+    create({ name, description }: ICreateCategoryDTO): void {
+        const categoy = new Category()
+
+        Object.assign(categoy, {
+            name,
+            description,
+            created_at: new Date()
+        })
+
+        this.categories.push(categoy)
+    }
+}
+
+export { CategoriesRepositories }
