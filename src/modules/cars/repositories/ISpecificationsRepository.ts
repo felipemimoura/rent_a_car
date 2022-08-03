b8fef2c6b1a6d7a95c14004c228a08208e4a153d
Felipe Moura <felipemimoura@gmail.com>
2022-08-02 21:40:06 -0300

    feat: create a new method into ICreateSpecificationRepostitory

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -1,3 +1,5 @@
+import { Specification } from "../model/Specification";
+
 interface ICreateSpecificationDTO {
     name: string;
     description: string;
@@ -6,6 +8,7 @@ interface ICreateSpecificationDTO {
 
 interface ICreateSpecificationsRepository {
     create({ name, description }: ICreateSpecificationDTO): void
+    findByName(name: string): Specification
 }
 
 export { ICreateSpecificationsRepository, ICreateSpecificationDTO }
