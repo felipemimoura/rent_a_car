2efb38999ce42e016e0968dba9b9d58ea91931f7
Felipe Moura <felipemimoura@gmail.com>
2022-08-09 22:10:43 -0300

    refactor: ajust in list categories

--- src/modules/cars/useCases/ListCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/ListCategories/ListCategoriesUseCase.ts
@@ -3,8 +3,8 @@ import { ICategoriesRespository } from "../../repositories/ICategoriesRepository
 
 class ListCategoriesUseCase {
     constructor(private categoriesRepository: ICategoriesRespository) { }
-    execute(): Category[] {
-        const categories = this.categoriesRepository.list()
+    async execute(): Promise<Category[]> {
+        const categories = await this.categoriesRepository.list()
 
         return categories
     }
