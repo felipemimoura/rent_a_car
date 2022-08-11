e14b05d2f780f74e146db93e66b854e494ad07a5
Felipe Moura <felipemimoura@gmail.com>
2022-08-11 19:57:12 -0300

    refactor: use tstringe to dependecy inject

--- src/modules/cars/useCases/ListCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/ListCategories/ListCategoriesController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
-    constructor(private listCategoriesUseCase: ListCategoriesUseCase) { }
 
     async handle(request: Request, response: Response): Promise<Response> {
-        const listAllCategories = await this.listCategoriesUseCase.execute()
+        const listAllCategoriesUseCase = container.resolve(ListCategoriesUseCase)
+        const listAllCategories = await listAllCategoriesUseCase.execute()
 
         return response.json(listAllCategories)
     }
