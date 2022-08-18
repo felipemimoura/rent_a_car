4b3cfa439fc42196a8284d05029ec89a3a174af3
Felipe Moura <felipemimoura@gmail.com>
2022-08-18 19:00:54 -0300

    refactor: docker run again

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import { categoriesRouter } from './categories.routes'
 import { specificationRouter } from './specification.routes'
 
-
 const router = Router()
 
 router.use("/categories", categoriesRouter)
