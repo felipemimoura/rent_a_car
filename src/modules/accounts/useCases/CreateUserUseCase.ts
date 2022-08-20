297b36edb0a34d97112efb6a945bc3ea9e0e563e
Felipe Moura <felipemimoura@gmail.com>
2022-08-20 12:12:27 -0300

    feat: crete useCase to Create user

---
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -0,0 +1,19 @@
+import { inject } from "tsyringe";
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+import { IUsersRepository } from "../repositories/IUsersRepository";
+
+class CreateUserUseCase {
+    constructor(
+        @inject("UserRepository")
+        private usersRepository: IUsersRepository
+    ) { }
+
+    async execute({ driver_license, email, name, password, username }: ICreateUserDTO): Promise<void> {
+        await this.usersRepository.create({
+            name, email, driver_license, password, username
+        })
+    }
+}
+
+
+export { CreateUserUseCase }
