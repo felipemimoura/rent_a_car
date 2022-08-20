99205c8b57bc858ed3e67110179adc62a1d3b0ef
Felipe Moura <felipemimoura@gmail.com>
2022-08-20 13:38:15 -0300

    feat: add injectavble to user usecase

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -1,7 +1,7 @@
-import { inject } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../repositories/IUsersRepository";
-
+@injectable()
 class CreateUserUseCase {
     constructor(
         @inject("UserRepository")
