219536db7fb2f9a75b6e4c1b82f3d54b5be0415d
Felipe Moura <felipemimoura@gmail.com>
2022-08-24 21:00:26 -0300

    feat: get file image to project

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -3,13 +3,14 @@ import { container } from "tsyringe";
 import { UpdateUserAvatarUseCase } from "./UpadateUserAvatarUseCase";
 
 class UpdateUserAvatarController {
-    async handle(request: Request, response: Response) {
+    async handle(request: Request, response: Response): Promise<Response> {
 
         const { id } = request.user
+        const avatar_file = request.file.filename
 
         const updateUserAvatarUseCase = container.resolve(UpdateUserAvatarUseCase)
 
-        await updateUserAvatarUseCase.execute({ user_id: id, avatar_file: null })
+        await updateUserAvatarUseCase.execute({ user_id: id, avatar_file: avatar_file })
 
 
         return response.status(204).send()
