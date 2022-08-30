fba9d3d9ec2664b6dacf8df4d3d7ea162c168098
Felipe Moura <felipemimoura@gmail.com>
2022-08-30 10:09:34 -0300

    test: create test to create Category

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,8 +1,40 @@
-describe("Create category", () => {
-    it('should be equal 4', () => {
-        const soma = 2 + 2
-        const resutado = 4
+import { AppError } from "../../../../errors/AppError"
+import { CategoriesRepositoryInMemory } from "../../repositories/in-momory/CategoriesRepositoryInMemory"
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase"
 
-        expect(soma).toBe(resutado)
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory
+let createCategoryUseCase: CreateCategoryUseCase
+
+describe("Create category", () => {
+    beforeEach(() => {
+        categoriesRepositoryInMemory = new CategoriesRepositoryInMemory()
+        createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory)
+    })
+
+    it('should be able to create a new category', async () => {
+
+        const category = {
+            name: "category teste",
+            description: "description teste"
+        }
+
+        await createCategoryUseCase.execute({ name: category.name, description: category.description })
+
+        const categoryCreated = await categoriesRepositoryInMemory.findByName(category.name)
+
+        expect(categoryCreated).toHaveProperty("id")
+    })
+
+    it("should be not able to create a new category with same name", async () => {
+        expect(async () => {
+            const category = {
+                name: "category teste",
+                description: "description teste"
+            }
+
+            await createCategoryUseCase.execute({ name: category.name, description: category.description })
+            await createCategoryUseCase.execute({ name: category.name, description: category.description })
+        }).rejects.toBeInstanceOf(AppError)
+
     })
 })
